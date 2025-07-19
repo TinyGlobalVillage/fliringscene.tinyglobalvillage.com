34586f38cccc2495388a0a79a01e77252fbbc70e
tinygvillage <connect@refusionist.com>
2025-07-19 16:55:05 -0500

    Remove .env.local and add to .gitignore

---
+++ src/components/ui/SignWrapper.tsx
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+export const SignWrapper = styled.div`
+  color: white;
+  min-height: 100vh;
+  font-family: 'Arial', sans-serif;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  text-align: center;
+  gap: 1.2rem;
+  padding: 2rem;
+`;
