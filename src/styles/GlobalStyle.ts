05dcea2867ff6fa918a71f370c7bc13a3c16f11a
tinygvillage <connect@refusionist.com>
2025-07-29 23:29:08 -0700

    deploy test 4

--- src/styles/GlobalStyle.ts
+++ src/styles/GlobalStyle.ts
@@ -4,49 +4,49 @@
 import { createGlobalStyle } from 'styled-components';
 
 const GlobalStyle = createGlobalStyle`
-  /* -----------------------------------------
-     Reset & Box Sizing
-  ----------------------------------------- */
-  *, *::before, *::after {
-    margin: 0;
-    padding: 0;
-    box-sizing: border-box;
-  }
+/* -----------------------------------------
+Reset & Box Sizing
+----------------------------------------- */
+*, *::before, *::after {
+margin: 0;
+padding: 0;
+box-sizing: border-box;
+}
 
-  /* -----------------------------------------
-     Base HTML & Body
-  ----------------------------------------- */
-  html, body, #__next {
-    width: 100%;
-    overflow-x: hidden;
-    font-family: 'Josefin Sans', sans-serif;
-  }
+/* -----------------------------------------
+Base HTML & Body
+----------------------------------------- */
+html, body, #__next {
+width: 100%;
+overflow-x: hidden;
+font-family: 'Josefin Sans', sans-serif;
+}
 
-  /* -----------------------------------------
-     Custom Font
-  ----------------------------------------- */
-  @font-face {
-    font-family: 'Josefin Sans';
-    src: url('/fonts/JosefinSans-VariableFont_wght.ttf') format('truetype');
-    font-weight: 100 700;
-    font-style: normal;
-    font-display: swap;
-  }
+/* -----------------------------------------
+Custom Font
+----------------------------------------- */
+@font-face {
+font-family: 'Josefin Sans';
+src: url('/fonts/JosefinSans-VariableFont_wght.ttf') format('truetype');
+font-weight: 100 700;
+font-style: normal;
+font-display: swap;
+}
 
-  /* -----------------------------------------
-     Links & Buttons
-  ----------------------------------------- */
-  a {
-    color: inherit;
-    text-decoration: none;
-  }
+/* -----------------------------------------
+Links & Buttons
+----------------------------------------- */
+a {
+color: inherit;
+text-decoration: none;
+}
 
-  a, button {
-    cursor: pointer;
-    background: none;
-    border: none;
-    font: inherit;
-  }
+a, button {
+cursor: pointer;
+background: none;
+border: none;
+font: inherit;
+}
 
 `;
 
