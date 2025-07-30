eaf1069d5de42aa64593c222f506fadca64f5714
tinygvillage <connect@refusionist.com>
2025-07-30 00:11:25 -0700

    testing5

--- src/app/(en)/_allPageComponents/buttons/NeonArrowSVGButton.tsx
+++ src/app/(en)/_allPageComponents/buttons/NeonArrowSVGButton.tsx
@@ -4,25 +4,25 @@ import CurveArrow from '../svg/CurveArrowSVG';
 // import StraightArrow from '../svg/StraightArrowSVG';
 
 const ButtonWrapper = styled.button`
-  all: unset;
-  display: block;
-  max-width: 500px;
-  width: 100%;
-  height; 48px;
-  cursor: pointer;
-  margin-top: -22px;
-  
-  transition: transform 0.2s ease, filter 0.2s ease;
+all: unset;
+display: block;
+max-width: 500px;
+width: 100%;
+height; 48px;
+cursor: pointer;
+margin-top: -22px;
 
-  &:hover {
-    /* slightly bigger */
-    transform: scale(1.05);
+transition: transform 0.2s ease, filter 0.2s ease;
 
-    /* crank the glow */
-    filter:
-      drop-shadow(0 0 1px #fe9e17)
+&:hover {
+/* slightly bigger */
+transform: scale(1.05);
 
-  }
+/* crank the glow */
+filter:
+drop-shadow(0 0 1px #fe9e17)
+
+}
 `;
 
 
@@ -30,18 +30,18 @@ function NeonArrowSVGButton() {
 
   return (
     <ButtonWrapper
-    aria-label="Scroll to upcoming show"
-    onClick={
-      () => {
-        const el = document.getElementById('upcoming-show');
-        if (el) {
-          el.scrollIntoView({ behavior: 'smooth', block: 'start' })
+      aria-label="Scroll to upcoming show"
+      onClick={
+        () => {
+          const el = document.getElementById('upcoming-show');
+          if (el) {
+            el.scrollIntoView({ behavior: 'smooth', block: 'start' })
+          }
         }
       }
-    }
     >
-      <CurveArrow/>
-{/* <StraightArrow/> */}
+      <CurveArrow />
+      {/* <StraightArrow/> */}
     </ButtonWrapper>
 
   );
