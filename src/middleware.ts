9191580cdab9c682e3b55527ae83b0f71516826d
tinygvillage <connect@refusionist.com>
2025-08-13 22:38:00 -0700

    changed middleware redirect to rewrite

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,14 +1,8 @@
 // src/middleware.ts
-import { NextRequest, NextResponse } from 'next/server';
-
-export function middleware(request: NextRequest) {
-  const { pathname } = request.nextUrl;
-  if (pathname === '/') {
-    const url = request.nextUrl.clone();
-    url.pathname = '/no';
-    return NextResponse.redirect(url, 308); // permanent
-  }
+import { NextResponse } from 'next/server';
+export function middleware(req: Request) {
+  const url = new URL(req.url);
+  if (url.pathname === '/') return NextResponse.rewrite(new URL('/no', url));
   return NextResponse.next();
 }
-
 export const config = { matcher: ['/'] };
